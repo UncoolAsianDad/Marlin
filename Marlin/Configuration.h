5f6c87c4f4a75efbfcb3b3d59e6122dc0d15ff70
Brian Ho <brian@codeforart.com>
2021-01-20 16:04:32 -0800

    Fixing BlTouch

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1087,7 +1087,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -50, -10, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { 0, 0, 45 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
