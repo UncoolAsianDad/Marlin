bae930fc578ddd5b17cd4d5552cf1f75df09897e
Brian Ho <brian@codeforart.com>
2025-12-22 18:36:34 -0800

    feat: adjust the height of the printer
    - 191mm is about the riht height

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -958,7 +958,7 @@
   #define DELTA_DIAGONAL_ROD 196.0        // (mm)
 
   // Distance between bed and nozzle Z home position
-  #define DELTA_HEIGHT 200                // (mm) Get this value from G33 auto calibrate
+  #define DELTA_HEIGHT 190                // (mm) Get this value from G33 auto calibrate
 
   #define DELTA_ENDSTOP_ADJ { 0.0, 0.0, 0.0 } // (mm) Get these values from G33 auto calibrate
 
